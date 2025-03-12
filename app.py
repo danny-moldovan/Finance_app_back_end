7c72050e89f53f4a0d8220b161af15059a52e3db
danny-moldovan <daniel.mda@gmail.com>
2025-03-12 07:34:52 +0000

    Changed port and removed the commit number form the endpoint URL

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def process_file(input_filename):
 
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5000, debug = True)
+    app.run(host = "0.0.0.0", port = 8080, debug = True)
