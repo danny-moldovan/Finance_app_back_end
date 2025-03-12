1122ba2a6eecda4d406d39090e24a933209c80b4
danny-moldovan <daniel.mda@gmail.com>
2025-03-12 20:08:24 +0000

    Copied the output file into workspace

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def generate_batch_summary():
         #print(str(os.path.exists(os.path.join('./data', output_filename))) if os.path.join('./data', output_filename) is not None else 'does not exist')
     
         if processing_result == "Request was successful" and output_filename is not None and os.path.exists(os.path.join('./data', output_filename)):
-            #os.system(f"cp {os.path.join('./data', output_filename)} ./data")
+            os.system(f"cp {os.path.join('./data', output_filename)} /workspace")
             return jsonify({"message": "Request was successful!"}), 200, {"Content-Type": "application/json"}
 
     except Exception as e:
